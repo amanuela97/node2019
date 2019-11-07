cc0b9e3b4b135d67da7ce4836325eeebcd31c2da
amanuela97 <babyzewdie@gmail.com>
2019-11-07 14:12:32 +0200

    index.js again

--- index.js
+++ index.js
@@ -4,19 +4,17 @@ const express = require('express');
 
 const app = express();
 
-app.get('/',(re,res) => {
+app.get('/',(req,res) => {
    res.send('Hello from my Node server');
 
 });
 
 app.get('/demo',(req,res) =>{
-    console.log()
-   res.send('demo'); 
+    console.log('request',req);
+    res.send('demo');
 });
 
 app.listen(3000,() =>{
     console.log('server app start?');
 });
-
-app.listen(3000);
 
