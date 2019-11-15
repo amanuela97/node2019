bce926eaece65ea60ac341dcda5f7f355eb21ecd
amanuela97 <babyzewdie@gmail.com>
2019-11-15 16:55:20 +0200

    latest change to index.js and ./model and db.js and env

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 'use strict';
 
 const express = require('express');
+const connection = require('./model/db.js');
 
 const app = express();
+
+app.use(express.static('public'));
+
+app.get('/animal', async (req,res) => {
+        try{
+        const [results, fields] = await connection.query(
+        'SELECT * FROM animal');
+        console.log(results); // results contains rows r
+        console.log(fields); // fields contains extra m
+        res.json(results);
+        }catch (e) {
+        console.log(e);
+        res.send('db error :(');
+        }
+
+});
 
 app.get('/',(req,res) => {
    res.send('Hello from my Node server');
